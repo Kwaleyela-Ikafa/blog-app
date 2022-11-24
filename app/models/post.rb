94a0508ec2dc91a64a4b62baf0448836e728f8ec
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-11-24 15:14:52 +0200

    Fix-linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-    has_many :comments, foreign_key: 'post_id'
-    has_many :likes, foreign_key: 'post_id'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 
-    def update_posts_count
-      author.increment!(:posts_counter)
-    end
-    
-    def five_recent_comments
-      comment.order('created_at DESC').limit(5)
-    end
+  def update_posts_count
+    author.increment!(:posts_counter)
+  end
+
+  def five_recent_comments
+    comment.order('created_at DESC').limit(5)
+  end
 end
