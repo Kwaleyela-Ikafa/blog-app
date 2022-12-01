135157a7f39084e06449b6e85f0c2c683fc62474
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-12-01 18:38:30 +0200

    Add App Views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_count
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
