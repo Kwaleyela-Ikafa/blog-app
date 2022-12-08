25dccdccbd80571def9ee7ef72afa89e0d92752d
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-12-08 15:29:28 +0200

    Add-Devise-Gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.includes(:author).find(params[:post_id])
     @post.comments.includes(:author).create(comment_parameters.merge(author_id: current_user.id))
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: :authenticated_root)
   end
 
   def comment_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # root :to => 'pages/home'
   # resources :users do
   #   resources :posts
   # end
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create]
@@ -14,7 +23,7 @@ Rails.application.routes.draw do
     resources :likes, only: [:create]
   end
   
-  root 'pages#home'
+  # root 'pages#home'
   # Defines the root path route ("/")
   # root "articles#index"
 end
